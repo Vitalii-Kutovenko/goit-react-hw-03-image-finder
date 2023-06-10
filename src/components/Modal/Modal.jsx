979af99efe8bcecf92048b49959039d51fa83b60
TyomaShapoval <shapoval1044@gmail.com>
2023-06-10 16:51:40 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,37 @@
-
-import React from 'react';
+// Modal.js
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Modal.module.css';
 
-const Modal = ({ largeImageURL, onClose }) => (
-  <div className={styles.overlay} onClick={onClose}>
-    <div className={styles.modal}>
-      <img src={largeImageURL} alt="" />
+const Modal = ({ largeImageURL, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  const handleOverlayClick = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className={styles.overlay} onClick={handleOverlayClick}>
+      <div className={styles.modal}>
+        <img src={largeImageURL} alt="" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Modal.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
